a6326aaca75f8c9c7d3d3a33ebaae89651fc1dc4
gbmatheus <gabrielmatheus2503@gmail.com>
2019-10-22 17:57:02 -0300

    Adicionando rotas do usuário e imovel

--- start/routes.js
+++ start/routes.js
@@ -14,8 +14,17 @@
 */
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+
 const Route = use('Route')
 
 Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
 })
+
+Route.post('/users', 'UserController.store')
+Route.post('/auth', 'AuthController.store')
+
+Route.resource('imoveis', 'ImovelController')
+  .apiOnly()
+  // .middleware('auth')
+
