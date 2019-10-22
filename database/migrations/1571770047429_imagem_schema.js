9245f1fc93570070ffecf8bb9b0190f0d0a91c5d
gbmatheus <gabrielmatheus2503@gmail.com>
2019-10-22 17:57:56 -0300

    Criração da imagem dos imoveis

---
+++ database/migrations/1571770047429_imagem_schema.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ImagemSchema extends Schema {
+  up () {
+    this.create('imagems', (table) => {
+      table.increments()
+      //relacionamento (imovel)um para (imagens)muitos, reponsável pela imagens de cada imovel
+      table.integer('imovel_id').unsigned().references('id').inTable('imovels').onUpdate('CASCADE').onDelete('CASCADE').notNullable()
+      table.string('path').notNullable()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('imagems')
+  }
+}
+
+module.exports = ImagemSchema
