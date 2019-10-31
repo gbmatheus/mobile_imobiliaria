98c1501c38fcc26d8da1025d4df7fbbbd46f4711
gbmatheus <gabrielmatheus2503@gmail.com>
2019-10-31 11:19:52 -0300

    Retirando comentários

--- app/Controllers/Http/ImovelController.js
+++ app/Controllers/Http/ImovelController.js
@@ -2,50 +2,17 @@
 
 const Imovel = use('App/Models/Imovel')
 
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
-
-/**
- * Resourceful controller for interacting with imovels
- */
 class ImovelController {
-  /**
-   * Show a list of all imovels.
-   * GET imovels
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
+
   async index ({ request, response, view }) {
     const imoveis = Imovel.all();
 
     return imoveis;
   }
 
-  
-  /**
-   * Create/save a new imovel.
-   * POST imovels
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
   async store ({ request, response }) {
   }
 
-  /**
-   * Display a single imovel.
-   * GET imovels/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
   async show ({ params, request, response, view }) {
     const imovel = await Imovel.findOrFail(params.id)
 
@@ -54,37 +21,9 @@ class ImovelController {
     return imovel
   }
 
-  /**
-   * Render a form to update an existing imovel.
-   * GET imovels/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
-
-  /**
-   * Update imovel details.
-   * PUT or PATCH imovels/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
   async update ({ params, request, response }) {
   }
 
-  /**
-   * Delete a imovel with id.
-   * DELETE imovels/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
   async destroy ({ params, request, response }) {
     const imovel = await Imovel.findOrFail(params.id)
 
