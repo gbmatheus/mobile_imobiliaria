24303d4e9cd3659e95d51360bb55fbb7f26a06b3
gbmatheus <gabrielmatheus2503@gmail.com>
2019-11-10 11:06:35 -0300

    Separando main e app

--- imobe_app/lib/main.dart
+++ imobe_app/lib/main.dart
@@ -1,30 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:imobe_app/src/pages/home_page.dart';
+import 'package:imobe_app/src/pages/imovel_detail_page.dart';
 import 'package:imobe_app/src/pages/login_page.dart';
-import 'package:imobe_app/src/pages/login_signup_page.dart';
+import 'package:imobe_app/src/pages/principal_page.dart';
+import 'package:imobe_app/src/pages/register_page.dart';
 import 'package:imobe_app/src/pages/signup_page.dart';
 import 'package:imobe_app/src/pages/forgot_page.dart';
 
 final Color colorWhite = Color.fromRGBO(250, 250, 250, 1.0);
 
-void main() => runApp(
-  MaterialApp(
-    title: 'Imobe',
-    theme: ThemeData(
-      brightness: Brightness.light,
-      primaryColor: Colors.white,
-      fontFamily: 'Montserrat'
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Imobe',
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primaryColor: Colors.purple,
+          fontFamily: 'Montserrat'),
+      home: ImobeApp(),
+      routes: <String, WidgetBuilder>{
+        '/login': (BuildContext context) => LoginPage(),
+        '/signup': (BuildContext context) => SignUpPage(),
+        '/forgot': (BuildContext context) => ForgotPage(),
+        '/home': (BuildContext context) => HomePage(),
+        '/register': (BuildContext context) => RegisterPage(),
+        '/principal': (BuildContext context) => PrincipalPage(),
+        '/detail-imovel/': (BuildContext context) => ImovelDetailPage(),
+      },
     ),
-    home: Scaffold(
-      // body: LoginSignUpPage(),
-      body: LoginPage(),
-    ),
-    routes: <String, WidgetBuilder> {
-      '/home': (BuildContext context) => HomePage(),
-      '/login': (BuildContext context) => LoginPage(),
-      '/signup': (BuildContext context) => SignUpPage(),
-      '/forgot': (BuildContext context) => ForgotPage(),
-    },
-  )
+  );
+}
 
-);
+class ImobeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ImovelDetailPage(),
+    );
+  }
+}
