726fe66dd5daf2548f042ffa9a5faed8269b5e86
gbmatheus <gabrielmatheus2503@gmail.com>
2019-11-10 11:11:36 -0300

    Criação dos itens

---
+++ imobe_app/lib/src/components/bottom_bar_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeAppItem extends StatefulWidget {
+  @override
+  _HomeAppItemState createState() => _HomeAppItemState();
+}
+
+class _HomeAppItemState extends State<HomeAppItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
