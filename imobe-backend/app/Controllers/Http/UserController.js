9870c8714189e686b704ba3e45a1ad7418f05805
gbmatheus <gabrielmatheus2503@gmail.com>
2019-12-03 00:10:23 -0300

    Habilitando cors e index de usuário

--- imobe-backend/app/Controllers/Http/UserController.js
+++ imobe-backend/app/Controllers/Http/UserController.js
@@ -3,6 +3,12 @@
 const User = use("App/Models/User");
 
 class UserController {
+  async index() {
+    const user = User.all()
+
+    return user
+  }
+
   async store({ request }) {
     const data = request.only(["username", "email", "password"])
 
--- imobe-backend/start/routes.js
+++ imobe-backend/start/routes.js
@@ -6,14 +6,21 @@ Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
 })
 
+// Route.get('/users', 'UserController.index')
 Route.post('/users', 'UserController.store')
 Route.post('/auth', 'AuthController.store')
 
 Route.resource('imoveis', 'ImovelController')
   .apiOnly()
-  .middleware('auth')
+  // .middleware('auth')
 
 Route.post('imoveis/:id/imagens', 'ImagemController.store')
   .middleware('auth')
 
 Route.get('imagens/:path', 'ImagemController.show')
+
+Route.resource('corretor', 'CorretorController')
+  .apiOnly()
+
+Route.resource('empresa', 'EmpresaController')
+  .apiOnly()
